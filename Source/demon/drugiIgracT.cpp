d13d4032aa479f8d5b5161cf2acc80d14a4f7273
brz1na <dpbrzi@gmail.com>
2017-08-25 00:09:10 +0200

    log_i rucne_izmene

--- Source/demon/drugiIgracT.cpp
+++ Source/demon/drugiIgracT.cpp
@@ -15,7 +15,7 @@ void UdrugiIgracT::PostInitProperties()
 	P2Keys.Add(EKeys::Down);
 	P2Keys.Add(EKeys::NumPadOne);
 	P2Keys.Add(EKeys::NumPadTwo);
-	
+	P2Keys.Add(EKeys::NumPadThree);
 }
 
 bool UdrugiIgracT::InputKey(FViewport* Viewport, int32 ControllerId, FKey Key, EInputEvent EventType, float AmountDepressed, bool bGamepad)
